fedf5b56a48b6713cea220974575f9a6deace153
Mahyar Ahmadpour <mahyar2012@gmail.com>
2024-03-29 22:22:03 +0100

    reinitialize with minimals

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 PLUGIN_NAME = 'redmine_fa_translation'
 
 Redmine::Plugin.register :redmine_fa_translation do
-  name 'Redmine FA Translation'
-  author 'Nahal'
-  description 'This plugin extends additional FA translations'
-  version '0.1.0'
-  url 'https://github.com/MahyarMehdi/redmine_fa_tanslation'
-  author_url 'https://nahal.app'
-
+  name 'Time entries limit by estimation'
+  author 'DigitalWand'
+  description 'This plugin restricts user to spend more time for a task then specifien in task\'s estimation'
+  version '1.0.0'
+  url 'https://github.com/DigitalWand/time_entries_limit_by_estimation'
+  author_url 'https://github.com/DigitalWand'
+  
 end
