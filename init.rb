a366394a7559755567788f8d90148e8b625514f2
Mahyar Ahmadpour <mahyar2012@gmail.com>
2024-05-19 16:45:23 +0200

    update github link

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_fa_translation do
   author 'Nahal Team'
   description 'This plugin provides additional info for FA support on RedMine both for the language and how it looks'
   version '1.0.0'
-  url 'https://github.com/MahyarMehdi/redmineFaTranslation'
+  url 'https://github.com/nahal-app/redmineFaTranslation'
   author_url 'https://nahal.app'
   
 end
